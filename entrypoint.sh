1b92bbd8fcd3175f027b1bcf16c2cc62378fd860
A.Rahman Amani <adjexpress@gmail.com>
2022-03-25 20:41:38 +0430

    personalize repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="5ae3f16f-7623-4174-bc46-956c9466ada3"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="5ae3f16f-7623-4174-bc46-956c9466ada3"
 fi
 echo ${Vmess_UUID}
 
